052c56d22ab881a69f1f8e2b334e60b648a4973e
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-09 11:56:44 +0530

    Add files via upload

---
+++ Day_13/Q52.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("Even Elements = %d\n", even);
+    printf("Odd Elements = %d", odd);
+
+    return 0;
+}
